0135f05a8e3e06b183ab15c38765c3181a8254b7
Shubham Bhatnagar <shubham.bhatnagar@pm.me>
2025-04-12 00:01:41 -0400

    tspomo

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+import { scrapeDepartment } from "~/lib/scrape-soc";
+
+export default function Page() {
+    const output = scrapeDepartment("PLCY");
+    return (
+      <main className="flex flex-col gap-4">
+        <h1 className="text-2xl font-semibold">Terpsicle</h1>
+        <p>Lorem ipsum dolor sit amet</p>
+      </main>
+    );
+  }
+  
