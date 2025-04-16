77b75d3a2d7270fe52c8c0a23f0e511d40f894a8
Zachary Robinson <me@zsrobinson.com>
2025-04-15 23:56:55 -0400

    intersection observer!

--- app/schedule/course-list.tsx
+++ app/schedule/course-list.tsx
@@ -7,7 +7,8 @@ import {
   PlusIcon,
   TriangleAlertIcon,
 } from "lucide-react";
-import { Dispatch, Fragment, SetStateAction, useState } from "react";
+import { Dispatch, Fragment, SetStateAction, useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { fetchCourses, fetchSections } from "./fetch";
@@ -20,18 +21,14 @@ export function CourseList({
   addedSections: AddedSection[];
   setAddedSections: Dispatch<SetStateAction<AddedSection[]>>;
 }) {
-  const [search, setSearch] = useState("CMSC");
-  const dept = search.slice(0, 4).toUpperCase();
+  const [search, setSearch] = useState("");
+  const dept = search.length >= 4 ? search.slice(0, 4).toUpperCase() : "";
 
   const coursesQuery = useQuery({
     queryKey: ["courses", dept],
     queryFn: () => fetchCourses({ dept_id: dept }),
-    initialData: [],
+    enabled: dept != "",
   });
-
-  const filteredCourses = coursesQuery.data.filter((c) =>
-    c.course_id.toLowerCase().startsWith(search.toLowerCase())
-  );
 
   return (
     <div className="flex flex-col gap-4 p-4 -m-4 overflow-y-scroll min-w-max pr-4">
@@ -45,14 +42,43 @@ export function CourseList({
         />
       </div>
 
-      {filteredCourses.map((course, i) => (
-        <CourseCard
-          course={course}
-          addedSections={addedSections}
-          setAddedSections={setAddedSections}
-          key={i}
-        />
-      ))}
+      {coursesQuery.isLoading && (
+        <span className="text-muted-foreground flex items-center gap-2 justify-center text-sm">
+          <LoaderCircleIcon className="animate-spin" size={16} /> Loading
+          Courses
+        </span>
+      )}
+
+      {coursesQuery.isError && (
+        <span className="text-destructive flex items-center gap-2 justify-center text-sm">
+          <TriangleAlertIcon size={16} /> Error Loading Courses
+        </span>
+      )}
+
+      {coursesQuery.data?.length === 0 && dept !== "" && (
+        <span className="text-muted-foreground flex items-center gap-2 justify-center text-sm">
+          No Courses
+        </span>
+      )}
+
+      {dept === "" && (
+        <span className="text-muted-foreground flex items-center gap-2 justify-center text-sm">
+          Search to View Courses
+        </span>
+      )}
+
+      {coursesQuery.data
+        ?.filter((c) =>
+          c.course_id.toLowerCase().startsWith(search.toLowerCase())
+        )
+        .map((course, i) => (
+          <CourseCard
+            course={course}
+            addedSections={addedSections}
+            setAddedSections={setAddedSections}
+            key={i}
+          />
+        ))}
     </div>
   );
 }
@@ -66,32 +92,46 @@ function CourseCard({
   addedSections: AddedSection[];
   setAddedSections: Dispatch<SetStateAction<AddedSection[]>>;
 }) {
+  const [ref, inView] = useInView({ rootMargin: "512px" });
+  const [hasBeenInView, setHasBeenInView] = useState(false);
+
+  useEffect(() => {
+    if (inView) setHasBeenInView(true);
+  }, [inView]);
+
   const sectionQuery = useQuery({
     queryKey: ["section", course.course_id],
     queryFn: () => fetchSections({ course_id: course.course_id }),
+    enabled: hasBeenInView,
   });
 
   return (
-    <div className="border rounded-lg p-2 w-sm">
+    <div className="border rounded-lg p-2 w-sm" ref={ref}>
       <span className="font-semibold leading-none pb-1 text-xl">
         {course.course_id}
       </span>
-      <span className="leading-none ml-1 text-muted-foreground text-sm italic">
+      <span className="leading-none ml-1.5 text-muted-foreground text-sm italic">
         {course.credits} Credit{course.credits !== 1 && "s"}
       </span>
-      <p className="leading-none text-balance">{course.name}</p>
+      <p className="leading-tight text-balance">{course.name}</p>
       <hr className="my-2" />
 
       {sectionQuery.isLoading && (
-        <span className="text-muted-foreground flex items-center gap-2 justify-center">
-          <LoaderCircleIcon className="animate-spin" size={20} /> Loading
+        <span className="text-muted-foreground flex items-center gap-2 justify-center text-sm">
+          <LoaderCircleIcon className="animate-spin" size={16} /> Loading
           Sections
         </span>
       )}
 
       {sectionQuery.isError && (
-        <span className="text-destructive flex items-center gap-2 justify-center">
-          <TriangleAlertIcon size={20} /> Error Loading Sections
+        <span className="text-destructive flex items-center gap-2 justify-center text-sm">
+          <TriangleAlertIcon size={16} /> Error Loading Sections
+        </span>
+      )}
+
+      {sectionQuery.data?.length === 0 && (
+        <span className="text-muted-foreground flex items-center gap-2 justify-center text-sm">
+          No Sections
         </span>
       )}
 
@@ -101,22 +141,32 @@ function CourseCard({
             <div className="flex justify-between items-center">
               <div>
                 <p>
-                  <span className="font-semibold">{sec.number}</span>:{" "}
+                  <span className="font-semibold">{sec.number}:</span>{" "}
                   {sec.instructors[0] ?? "Instructor TBA"}
                 </p>
                 {sec.meetings.map((time, i) =>
                   time.start_time === undefined ||
                   time.end_time === undefined ? (
-                    <p key={i}>ONLINE ASYNC</p>
+                    <p
+                      className="text-muted-foreground text-sm leading-tight"
+                      key={i}
+                    >
+                      ONLINE ASYNC
+                    </p>
                   ) : (
-                    <p key={i}>
+                    <p
+                      className="text-muted-foreground text-sm leading-tight"
+                      key={i}
+                    >
                       {time.days} {formatTime(time.start_time)}–
                       {formatTime(time.end_time)} {time.building} {time.room}{" "}
                       {time.classtype}
                     </p>
                   )
                 )}
-                {sec.open_seats}/{sec.seats} seats, {sec.waitlist} waitlist
+                <p className="text-muted-foreground text-sm leading-tight">
+                  {sec.open_seats}/{sec.seats} seats, {sec.waitlist} waitlist
+                </p>
               </div>
 
               {addedSections.some((str) => str === sec.section_id) ? (
